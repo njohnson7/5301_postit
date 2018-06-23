cb191ec2a4e7606300bd5b05f2662775f5f63a7d
Nick Johnson <njohnson7@users.noreply.github.com>
2018-06-22 23:39:30 -0700

    added forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,25 +1,46 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update]
+
   def index
     @posts = Post.all
   end
 
   def show
-    @post = Post.find params[:id]
   end
 
   def new
-
+    @post = Post.new
   end
 
   def create
-
+    @post = Post.new post_params
+    if @post.save
+      flash[:notice] = 'Post successfully created'
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
 
   def edit
-
   end
 
   def update
+    if @post.update post_params
+      flash[:notice] = 'Post has been updated'
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
 
+  private
+
+  def post_params
+    params.require(:post).permit!
+  end
+
+  def find_post
+    @post = Post.find params[:id]
   end
 end
