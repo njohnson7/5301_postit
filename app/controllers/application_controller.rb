c52dba35ee22d0e842f78e94c3cdbf2963e02fa8
Nick Johnson <njohnson7@users.noreply.github.com>
2018-06-27 09:40:47 -0700

    added admin role

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :current_user, :logged_in?
+  helper_method :current_user, :logged_in?, :admin?
 
   def current_user
     @current_user ||= session && session[:user_id] && User.find_by(slug: session[:user_id])
@@ -13,11 +13,22 @@ class ApplicationController < ActionController::Base
     !!current_user
   end
 
+  def admin?
+    logged_in? && current_user.role == 'admin'
+  end
+
   private
 
   def require_user
     unless logged_in?
-      flash[:error] = 'You must be logged in to do that.'
+      flash[:error] = 'You must be logged in to do that'
+      redirect_to root_path
+    end
+  end
+
+  def require_admin
+    unless admin?
+      flash[:error] = 'You must be an admin to do that'
       redirect_to root_path
     end
   end
