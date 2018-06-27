2647a2dd9bbb1ac12cd81059e58a225b96c2e1d6
Nick Johnson <njohnson7@users.noreply.github.com>
2018-06-27 00:26:19 -0700

    extracted voteable to module

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,27 +1,17 @@
 class Post < ActiveRecord::Base
+  include Voteable
+
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   has_many   :comments
   has_many   :post_categories
   has_many   :categories, through: :post_categories
-  has_many   :votes, as: :voteable
+
 
   validates :title,       presence: true, length: { minimum: 3 }
   validates :url,         presence: true  #, uniqueness: true
   validates :description, presence: true
 
   before_save :slugify
-
-  def total_votes
-    up_votes - down_votes
-  end
-
-  def up_votes
-     self.votes.where(vote: true).size
-  end
-
-  def down_votes
-    self.votes.where(vote: false).size
-  end
 
   def slugify
     slug = self.title.strip.downcase.gsub(/[^a-z0-9]/i, '-').gsub(/-+/, '-')
